4c024dc92a818bf77ee44e6e6cc30f39e54a31f9
Scott Stults <sstults@o19s.com>
2016-07-21 05:57:42 -0400

    Add copyright notice

--- src/test/java/com/boundlessgeo/GeoHeatmapPluginTest.java
+++ src/test/java/com/boundlessgeo/GeoHeatmapPluginTest.java
@@ -14,15 +14,10 @@
  *
  */
 
-package com.boundlessgeo.module;
+package com.boundlessgeo;
 
-import com.boundlessgeo.service.GeoHeatmapService;
-import org.elasticsearch.common.inject.AbstractModule;
-
-public class GeoHeatmapModule extends AbstractModule {
-
-    @Override
-    protected void configure() {
-        bind(GeoHeatmapService.class).asEagerSingleton();
-    }
+/**
+ *
+ */
+public class GeoHeatmapPluginTest {
 }
